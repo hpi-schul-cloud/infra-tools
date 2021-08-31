47f257acbbb74a14cf52d633fc2d81189a896b55
Lars Dahlke <lars.dahlke@gmx.de>
2021-08-31 19:24:41 +0200

    Added more parameter handling

--- sc-tunnel/sct.py
+++ sc-tunnel/sct.py
@@ -19,19 +19,13 @@ def parseArguments():
     '''
     Parses the program arguments and returns the data parsed by argparse.
     '''
-    parser = argparse.ArgumentParser(description='Tunnels access to IONOS Kubernets cluster via a jump host.')
+    parser = argparse.ArgumentParser(description='Tunnels access to IONOS Kubernetes cluster via a jump host.')
 
     parser.add_argument('--version', action='version', version='1.0.0')
-    # parser.add_argument("-sc", "--showconfig", action='store_true', help = "Prints out the configuration that will be used, before any other action.")
-    #parser.add_argument("-d", "--dailyincrement", action='store_true', help = "Creates a backup of the files that were uploaded during the last day.")
-    # parser.add_argument("-s", "--syncfull", action='store_true', help = "Synchronizes the full backup of the configured instances, if scheduled for today.")
-    # parser.add_argument("-va", "--validate", action='store_true', help = "Validates the existing syncfull backup. The validation checks the number of objects and the size of the buckets.")
-    # parser.add_argument("-i", "--instance", action='append', dest = 'instances_to_backup', help = "Limits the scope to the specified instance. Add the name of an instance to backup as argument.")
-    # parser.add_argument("-c", "--configuration", help = "Name of a yaml configuration file to use for the backup. The configuration file contains the definition of the available instances and other static configuration data.", default="s3b_test.yaml")
-    # parser.add_argument("-f", "--force", action='store_true', help = "Force. If -s is specified forces a syncfull backup, even if it is not scheduled for today.")
-    # parser.add_argument("-w", "--whatif", action='store_true', help = "If set no write operations are executed. rclone operations are executed with --dryrun.")
-    # parser.add_argument("-ra", "--restoreall", action='store', dest = 'restore_instance', help = "Restores all buckets from a backup set. The parameter rb must be set additionally. The instance drive where the restore will happen must be empty.")
-    # parser.add_argument("-rb", "--restorebackupset", action='store', dest = 'restore_backupset', help = "Use together with -ra. Selects the backupset to restore. E.g. 'demo-full-0'")
+    parser.add_argument("--list", action='store_true', help = "Lists the locally available IONOS K8S cluster to be used for tunneling.")
+    parser.add_argument("--update", action='store_true', help = "Update the locally available IONOS K8S clusterin $(HOME)/.kube/.")
+    #parser.add_argument("--cluster", dest='cluster_names', nargs='+', action='append', required=False, default='', help = "One or more cluster names to open  a tunnel for, e.g. sc-prod-admin.")
+    parser.add_argument("--cluster", required=False, default='', help = "Cluster name to open  a tunnel for, e.g. sc-prod-admin.")
     args = parser.parse_args()
     return args
 
@@ -49,8 +43,8 @@ if __name__ == '__main__':
         parsedArgs = parseArguments()
         configuration_file = 'sct_config.yaml'
         sct_tunnel_config = read_configuration(configuration_file)
-        api_server = sct_tunnel_config.clusters['sc-prod-admin']
-        openTunnel(sct_tunnel_config.jumphost, sct_tunnel_config.jumphost_user, api_server.api_server_host, api_server.api_server_port)
+        #api_server = sct_tunnel_config.clusters['sc-prod-admin']
+        #openTunnel(sct_tunnel_config.jumphost, sct_tunnel_config.jumphost_user, api_server.api_server_host, api_server.api_server_port)
         exit(0)
     except Exception as ex:
         logging.exception(ex)
