a6ce5fe7ceae17c368572c3af23beaf705f4b852
Lars Dahlke <lars.dahlke@gmx.de>
2021-09-02 20:01:28 +0000

    Completed hostname modul

--- sc-tunnel/sct_logic/hostname.py
+++ sc-tunnel/sct_logic/hostname.py
@@ -13,6 +13,30 @@ def listHosts():
             print(entry)
     print("\n")
 
+def addHost(hostname):
+    '''
+    Add "hostname" to the hosts files as alias for 127.0.0.1
+    '''
+    hosts = Hosts()
+    new_entry = HostsEntry(entry_type='ipv4', address='127.0.0.1', names=[hostname])
+    hosts.add([new_entry])
+    hosts.write()
+
+def removeHost(hostname):
+    '''
+    Remove "hostname" from hosts file
+    '''
+    hosts = Hosts()
+    if hosts.exists(address='127.0.0.1',names=[hostname]):
+        hosts.remove_all_matching(name=hostname)
+        hosts.write()
+    
 if __name__ == '__main__':
+    listHosts()
+    addHost('karl.dall')
+    addHost('hugo.egon.balder')
+    listHosts()
+    removeHost('hugo.egon.balder')
+    removeHost('karl.dall')
     listHosts()
     pass
