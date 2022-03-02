503654378f5e5dd5986243e1a997e3671f52f90e
Aimee Weiss <aimee.weiss@capgemini.com>
2022-03-02 11:22:31 +0100

    chnage vars

--- onepwd/plugins/action/action.py
+++ onepwd/plugins/action/action.py
@@ -32,20 +32,25 @@ class ActionModule(ActionBase):
         session_timeout=kwargs.get('session_timeout', 30)
         op = onepwd.OnePwd(secret=login_secret, shorthand=session_shorthand, session_timeout=session_timeout)
         # Get/Set Values
-        category=kwargs.get('category', 'password')
-        title=kwargs.get('secret_name', None)
-        vault=kwargs.get('vault', None)
-        url=kwargs.get('url')
-        content=kwargs.get('content', '')
+
+        # works local
+        category = task_vars['category']
+        title = task_vars['secret_name']
+        vault = task_vars['vault']
+        content = task_vars['content']
+        # category=kwargs.get('category', 'password')
+        # title=kwargs.get('secret_name', None)
+        # vault=kwargs.get('vault', None)
+        # content=kwargs.get('content', '')
         template = '{"fields":[],"notesPlain":"","passwordHistory":[],"sections":[{"fields":[' + str(content) +  ']}]}'
         encoded_item = url64.encode(template)      
         # Upload Secret
         # onepwd.OnePwd.create_item(op, category, encoded_item, title, vault=vault, url=url)
         # print(onepwd.OnePwd.create_item(op, category, encoded_item, title, vault=vault, url=url))
-        command = onepwd.OnePwd.create_item(op, category, encoded_item, title, vault=vault, url=url)
-        # onepwd.OnePwd.create_item(op, category, encoded_item, title, vault=vault, url=url)
+        command = onepwd.OnePwd.create_item(op, category, encoded_item, title, vault=vault)
+        # onepwd.OnePwd.create_item(op, category, encoded_item, title, vault=vault)
         return command
 
 
-# ActionModule.run('schulcloud.onepwd.onepwd.OnePwd', secret_name='TestIte99', vault='infra-dev', url='ionos@mail.com', content='{"k":"string","n":"Bucket-ID","t":"Bucket-id","v":"NBC-Test"},{"k":"concealed","n":"password","t":"Access-Key","v":"489570283475"},{"k":"concealed","n":"password2","t":"Access-Secret","v":"102947019"}')
+# ActionModule.run('schulcloud.onepwd.onepwd.OnePwd', task_vars={'category': 'password', 'secret_name': 'TestIte99', 'vault': 'infra-dev', 'content': '{"k":"string","n":"Bucket-ID","t":"Bucket-id","v":"NBC-Test"},{"k":"concealed","n":"password","t":"Access-Key","v":"489570283475"},{"k":"concealed","n":"password2","t":"Access-Secret","v":"102947019"}'})
 
