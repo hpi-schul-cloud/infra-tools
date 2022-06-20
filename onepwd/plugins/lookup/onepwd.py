b83d9faca1f8283bf2f82dec595ff8a3575edadb
Dimitrij Pinneker <dimitrij.pinneker@capgemini.com>
2022-06-20 13:45:15 +0200

    OPS-3293 Added debug output

--- onepwd/plugins/lookup/onepwd.py
+++ onepwd/plugins/lookup/onepwd.py
@@ -15,8 +15,12 @@ from subprocess import Popen, PIPE
 from ansible.errors import AnsibleError
 from ansible.module_utils._text import to_bytes, to_text
 from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
 import os
 import onepwd
+
+display = Display()
 
 # https://docs.ansible.com/ansible/latest/dev_guide/developing_plugins.html#lookup-plugins
 
@@ -30,6 +34,14 @@ class LookupModule(LookupBase):
         secret_name=kwargs.get('secret_name', '')
         vault=kwargs.get('vault', None)
         field=kwargs.get('field', None)
+        display.vvvv(u"Session shorthand is %s" % session_shorthand)
+        display.vvvv(u"Secret name is %s" % secret_name)
+        display.vvvv(u"vault is %s" % vault)
+        display.vvvv(u"field is %s" % field)
+        display.vvvv(u"OP_EMAIL is %s" % os.environ.get("OP_EMAIL"))
+        display.vvvv(u"OP_PASSWORD is %s" % os.environ.get("OP_PASSWORD"))
+        display.vvvv(u"OP_SUBDOMAIN is %s" % os.environ.get("OP_SUBDOMAIN"))
+        display.vvvv(u"OP_SECRET_KEY is %s" % os.environ.get("OP_SECRET_KEY"))
         values=[]
         values.append(onepwd.get_single_secret(op, secret_name, field=field, vault=vault))
         return values
