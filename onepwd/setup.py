13de3b2e4680eba1756c293ff8fe4a41436adc78
YannickEvers <112076936+YannickEvers@users.noreply.github.com>
2022-09-20 13:59:39 +0200

    Update setup.py

--- onepwd/setup.py
+++ onepwd/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="onepwd",
-    version="1.0.6",
+    version="2.0.0",
     author="HPI Schulcloud",
     author_email="devops@hpi-schul-cloud.de",
     description="Utilities to work with 1password",
