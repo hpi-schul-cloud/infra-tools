397c619fa84eaec4415754f699ba2f0098db81b3
Dimitrij Pinneker <dimitrij.pinneker@capgemini.com>
2022-10-25 19:26:45 +0200

    Ops 3293 awx poc (#45)
    * OPS-3293 Added debug output
    
    * OPS-3293 Added debug output
    
    * OPS-3293 Added twofactor digits output
    
    * OPS-3293 Updated op to 1.12.5
    
    * OPS-3293 Set timezone again
    
    * OPS-3822 Updated op cli

--- onepwd/plugins/lookup/onepwd.py
+++ onepwd/plugins/lookup/onepwd.py
@@ -15,8 +15,12 @@ from subprocess import Popen, PIPE
 from ansible.errors import AnsibleError
 from ansible.module_utils._text import to_bytes, to_text
 from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
+
 import os
 import onepwd
+
+display = Display()
 
 # https://docs.ansible.com/ansible/latest/dev_guide/developing_plugins.html#lookup-plugins
 
@@ -26,6 +30,14 @@ class LookupModule(LookupBase):
         login_secret=onepwd.get_op_login()
         session_shorthand=kwargs.get('session_shorthand', os.getenv('USER'))
         session_timeout=kwargs.get('session_timeout', 30)
+        display.debug(u"Session shorthand is %s" % session_shorthand)
+        display.debug(u"Secret name is %s" % kwargs.get('secret_name', ''))
+        display.debug(u"vault is %s" % kwargs.get('vault', None))
+        display.debug(u"field is %s" % kwargs.get('field', None))
+        display.debug(u"OP_EMAIL is %s" % os.environ.get("OP_EMAIL"))
+        display.debug(u"OP_PASSWORD is %s" % os.environ.get("OP_PASSWORD"))
+        display.debug(u"OP_SUBDOMAIN is %s" % os.environ.get("OP_SUBDOMAIN"))
+        display.debug(u"OP_SECRET_KEY is %s" % os.environ.get("OP_SECRET_KEY"))
         op = onepwd.OnePwd(secret=login_secret, shorthand=session_shorthand, session_timeout=session_timeout)
         secret_name=kwargs.get('secret_name', '')
         vault=kwargs.get('vault', None)
