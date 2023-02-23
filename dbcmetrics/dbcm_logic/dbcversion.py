0d3b6123e2f361db735ec5aff5f2e95aaae02df4
YannickEvers <112076936+YannickEvers@users.noreply.github.com>
2023-02-23 15:33:07 +0100

    OPS-4323-fix-dbcmetrics-new-versions-format (#53)
    Change dbcmetrics to be compatible with the new output format of the /version endpoint.
    Components/Services are not defined in the config anymore, but are dynamically extracted from the version output.

--- dbcmetrics/dbcm_logic/dbcversion.py
+++ dbcmetrics/dbcm_logic/dbcversion.py
@@ -2,6 +2,7 @@
 Modul for a class that query the configured dBildungscloud instances for their known service versions and provide the version via Prometheus compatible metrics.
 '''
 import logging
+import re
 from time import sleep
 import threading, requests, json, urllib.parse
 from typing import Dict
@@ -9,14 +10,14 @@ from typing import List
 from dbcm_common.dbcmexception import DBCMException
 from prometheus_client import Info
 from dbcm_data.dbcm_instance import DBCMInstance
-from dbcm_data.dbcm_version import DBCMVersion
 
 class VersionMetricsThreading(object):
 
     def __init__(self, configuration_content: dict):
         try:
             version_data = configuration_content['version_metrics']
-            self.version = DBCMVersion(version_data['services'], version_data['interval'])
+            self.endpoint = version_data['endpoint']
+            self.interval = version_data['interval']
         except:
             logging.error("Missing or wrong 'version_metrics' value in configuration file.")
             raise DBCMException
@@ -33,35 +34,35 @@ class VersionMetricsThreading(object):
             logging.error("Missing or wrong 'instances' value in configuration file.")
             raise DBCMException
 
-        label_names = ['app_instance', 'dashboard'] + list(self.version.services.keys())
-        self.pmc_infos: Info = Info('version', 'Version Information', label_names)
+        self.pmc_infos: Info = Info('version', 'Version Information')
         self.thread = threading.Thread(target=self.run)
         self.thread.daemon = True
         self.thread.start()
 
     def run(self):
         while True:
-            self.pmc_infos.clear()
             for i in self.instances:
-                labels: Dict = self.get_instance_versions(i)
-                labels['app_instance'] = i.name
-                labels['dashboard'] = 'version_dashboard'
-                self.pmc_infos.labels(**labels)
-            sleep(self.version.interval)
+                info: Dict = self.get_instance_versions(i)
+                info['app_instance'] = i.name
+                info['dashboard'] = 'version_dashboard'
+                self.pmc_infos.info(info)
+            sleep(self.interval)
 
     def get_instance_versions(self, instance: DBCMInstance):
-        # Loop over all services of the give instance, gather the version and store them in the returned dictionary
+        # Get the version info, loop over all entries/components and 
+        # if a version exists, store it in the returned dictionary.
         labels: Dict = {}
-        for service_name, service_suffix in self.version.services.items():
-            instance_url = instance.url
-            fvurl = urllib.parse.urljoin(instance_url, service_suffix)
-            try:
-                url = requests.get(fvurl)
-                text = url.text
-                data = json.loads(text)
-                logging.info("{} version of {} is: {}".format(service_name, instance.name, data['version']))
-                labels[service_name] = data['version']
-            except:
-                logging.error("Get {} version of {} failed!".format(service_name, instance.name))
-                labels[service_name] = ""
+        try:
+            version_url = urllib.parse.urljoin(instance.url, self.endpoint)
+            response = requests.get(version_url)
+            resp_json = json.loads(response.text)
+            for component_name, component_info in resp_json.items():
+                if isinstance(component_info, dict) and "version" in component_info:
+                    version = component_info["version"]
+                    # Only alphanumerics and underscores allowed
+                    name_sanitized = re.sub("\\W+", "_", component_name)
+                    labels[name_sanitized] = version
+                    logging.info(f"{component_name} version of {instance.name}: {version}")
+        except:
+                logging.error("Getting versions for {} failed!".format(instance.name))
         return labels
