37d4643ac49033791530e3a81d351e6efb38df24
Martin Jürgens <martin.juergens@capgemini.com>
2025-10-16 11:49:24 +0200

    per monitor kuma metrics

--- dbcmetrics/dbcm_logic/dbcuptimekumamaintenance.py
+++ dbcmetrics/dbcm_logic/dbcuptimekumamaintenance.py
@@ -30,19 +30,20 @@ class UptimeKumaMaintenanceWindowThreading(object):
         raise DBCMException
 
       self.METRIC_REFRESH_INTERVAL = datetime.timedelta(seconds=file_configs["metric_refresh_interval_sec"])
-      self.STATUS_PAGE_SLUG = file_configs["status_page_slug"]
 
     except Exception as e:
       logging.error("Missing or invalid configuration for Uptime Kuma API maintenance metrics.")
       raise DBCMException from e
 
     # stores maintenance windows.
-    self.windows = []
+    self.windows = {}
+    # stores monitor ids and corresponding names
+    self.monitors = {}
     # Prometheus gauge metric
     self.metric = Gauge(
       "uptime_kuma_maintenance_active",
       "1 if any monitor is in a scheduled maintenance window, else 0",
-      ["slug"]
+      ["monitor_id", "monitor_name"]
     )
 
     # Loads initial data, starts a background thread to refresh metrics periodically.
@@ -61,25 +62,25 @@ class UptimeKumaMaintenanceWindowThreading(object):
   # Fetches maintenance data from Uptime Kuma API.
   def load_maintenance_windows(self):
     try:
+      windows_temp = {}
+
       with UptimeKumaApi(self.API_URL) as api:
         api.login(self.UPTIME_KUMA_USERNAME, self.UPTIME_KUMA_PASSWORD)
-        maintenances = api.get_maintenances()
-        status_page_groups = api.get_status_page(self.STATUS_PAGE_SLUG).get('publicGroupList')
-        status_page_monitor_ids = {monitor['id'] for entry in status_page_groups for monitor in entry['monitorList']}
 
-        self.windows.clear()
+        for monitor in api.get_monitors():
+          windows_temp[monitor.get("id")] = []
+          self.monitors[monitor.get("id")] = monitor.get("name")
 
         def is_active_and_not_ended(item):
           return item.get("active") and item.get("status") != "ended"
 
+        maintenances = api.get_maintenances()
         filtered_maintenances = list(filter(is_active_and_not_ended, maintenances))
 
         for maintenance in filtered_maintenances:
+          monitors = api.get_monitor_maintenance(maintenance.get("id"))
 
-          # Only process the maintenance element if it is relevant to the status page configured via the slug
-          if any(
-            monitor.get("id") in status_page_monitor_ids for monitor in api.get_monitor_maintenance(maintenance.get("id"))):
-
+          for monitor in monitors:
             tz = ZoneInfo(maintenance.get("timezone"))
             for slot in maintenance.get("timeslotList", []):
               start = parse(slot["startDate"])
@@ -90,8 +91,11 @@ class UptimeKumaMaintenanceWindowThreading(object):
               if end.tzinfo is None:
                 end = end.replace(tzinfo=tz)
 
-              self.windows.append((start, end))
-              logging.info(f"Loaded {len(self.windows)} maintenance windows.")
+              windows_temp[monitor.get("id")].append((start, end))
+            logging.info(f"Loaded {len(windows_temp)} maintenance windows for monitor {monitor.get("id")}.")
+
+        # only set self.windows when all api calls have been successful
+        self.windows = windows_temp
 
     except Exception as e:
       logging.error("Error loading maintenance windows from API.")
@@ -100,5 +104,9 @@ class UptimeKumaMaintenanceWindowThreading(object):
   # Checks if current time is within any maintenance window and sets the metric accordingly.
   def refresh_metrics(self):
     now = datetime.datetime.now(datetime.UTC)
-    in_window = any(start <= now <= end for start, end in self.windows)
-    self.metric.labels(slug=self.STATUS_PAGE_SLUG).set(1 if in_window else 0)
+
+    for monitor_id, monitor_windows in self.windows.items():
+      if any(start <= now <= end for start, end in monitor_windows):
+        self.metric.labels(monitor_id=monitor_id,monitor_name=self.monitors[monitor_id]).set(1)
+      else:
+        self.metric.labels(monitor_id=monitor_id,monitor_name=self.monitors[monitor_id]).set(0)
